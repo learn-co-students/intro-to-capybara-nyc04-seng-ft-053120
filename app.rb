0f4b5ddd758479411045dcdba770aa59d73379ef
Rouan Thompson <rouan.thompson@gmail.com>
2020-07-15 22:02:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class Application < Sinatra::Base
   # Write your code here!
 
+  #responds to get request(from user) for '/'
+  get '/' do
+    #renders the index file
+    erb :index
+  end
+
+  #responds to a post request from user for /greet
+  post '/greet' do
+    erb :greet
+  end
 end
